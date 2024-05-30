7884bdb0c70d6d4d0a0c573ed35d7c7db60319c2
MarcOlivier91 <92017823+MarcOlivier91@users.noreply.github.com>
2024-05-30 04:30:09 +0200

    maquette + header

---
+++ lib/data.ts
@@ -0,0 +1,52 @@
+export const links = [
+    {
+        name: "Home",
+        route: "#home"
+    },
+    {
+        name: "Intro",
+        route: "#intro"
+    },
+    {
+        name: "Projets",
+        route: "#projets"
+    },
+    {
+        name: "Contact",
+        route: "#contact"
+    },
+] as const;
+
+export const intro = "Ceci est une introduction me décrivant et présentant mon parcours.." as const;
+
+export const projectsData = [
+    {
+        title: "AniCorner",
+        description: "Ceci est un site rassemblant les dernières nouveautés et sorties des animes et mangas.",
+        tags: ["React", "Next.js", "MongoDB", "Express.js"],
+        //imageUrl: aniCornerImg,
+    },
+
+    {
+        title: "WeathAir",
+        description: "Ceci est une application mobile donnant des informations sur la météo.",
+        tags: ["Flutter"],
+        //imageUrl: weathairIng,
+    },
+] as const;
+
+export const skills = [
+    "HTML",
+    "CSS",
+    "Tailwind CSS",
+    "Javascript",
+    "Typescript",
+    "React",
+    "Next.js",
+    "Express",
+    "MongoDB",
+    "MySQL",
+    "Python",
+    "PHP",
+    "Git"
+] as const;
